3015097869d137bc76546c2d995e792652749403
Africanus Chambula <davidchrispine61977000@gmail.com>
2025-07-05 15:43:27 +0300

    modified added column

--- database/migrations/2025_06_20_204709_create_students_table.php
+++ database/migrations/2025_06_20_204709_create_students_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('s_id')->unique();
+            $table->string('photo')->nullable();
             $table->string('name');
             $table->string('gender');
             $table->string('company');
